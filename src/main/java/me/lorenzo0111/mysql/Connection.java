07e9af2f2f4b39eb7e1d193ef6c38a5236769481
Lorenzo0111 <lorenzo@rocketplugins.space>
2021-01-29 16:51:44 +0100

    Created Connection

---
+++ src/main/java/me/lorenzo0111/mysql/Connection.java
@@ -0,0 +1,43 @@
+package me.lorenzo0111.mysql;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SuppressWarnings("unused")
+public class Connection {
+    private final java.sql.Connection connection;
+
+    /**
+     * @param connection java.sql Connection to me.lorenzo0111 Connection
+     */
+    public Connection(java.sql.Connection connection) {
+        this.connection = connection;
+    }
+
+    /**
+     * @param host MySQL server host
+     * @param port MySQL server port
+     * @param database MySQL database
+     */
+    public Connection(String host, int port, String database) throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database);
+    }
+
+    /**
+     * @param host MySQL server host
+     * @param port MySQL server port
+     * @param database MySQL database
+     * @param username MySQL server username
+     * @param password MySQL server password
+     */
+    public Connection(String host, int port, String database, String username, String password) throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, username, password);
+    }
+
+    /**
+     * @return java.sql Connection
+     */
+    public java.sql.Connection getConnection() {
+        return connection;
+    }
+}
